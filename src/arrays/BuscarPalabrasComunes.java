b1ea0c6b8c513a6151e86a7cdd1f222d2b550023
FacilitadorDAM <145330715+ProfeFS@users.noreply.github.com>
2023-10-26 17:22:41 +0200

    Update BuscarPalabrasComunes.java

--- src/arrays/BuscarPalabrasComunes.java
+++ src/arrays/BuscarPalabrasComunes.java
@@ -4,17 +4,7 @@ public class BuscarPalabrasComunes {
 
 	public static void main(String[] args) {	
 
-		String[] prov1 = { "Madrid", "Barcelona", "Sevilla", "Toledo", "Valencia" };
-		String[] prov2 = { "Alicante", "Valencia", "Asturias", "Salamanca", "Madrid" };
-
-		System.out.println("Las provincias comunes en las dos listas son: ");
-		for (int i = 0; i < prov1.length; i++) {
-			for (int j = 0; j < prov2.length; j++) {
-				if (prov1[i].equalsIgnoreCase(prov2[j])) {
-					System.out.println(prov1[i]);
-				}
-			}
-		}
+		
 	}
 
 
